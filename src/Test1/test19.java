a891e44617bd1db210a71e014722795f4798ed8f
Amarprit <amarpritkaur.kharbanda@citrix.com>
2024-02-18 18:45:32 +0530

    practice-2 sample code

---
+++ src/Test1/test19.java
@@ -0,0 +1,10 @@
+package src.Test1;
+
+public class test19 {
+    public static void main(String[] args) {
+        boolean flag = false;
+        if (!flag) {
+            System.out.println("Hello world");
+        }
+    }
+}
