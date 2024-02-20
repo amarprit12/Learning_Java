06e50bc7bfe08f5fa7689da8fc928de1134cf1cd
Amarprit <amarpritkaur.kharbanda@citrix.com>
2024-02-20 14:26:17 +0530

    practice-6 sample code

---
+++ Test2/test10.java
@@ -0,0 +1,28 @@
+package Test2;
+
+import java.util.Scanner;
+
+public class test10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num;
+        int sum = 0;
+//        int i = 0;
+//        while (i >= 0) {
+//            num = scanner.nextInt();
+//            sum = sum + num;
+//            i++;
+//            if (num < 0) {
+//                break;
+//            }
+//        }
+//        System.out.println(sum);
+        do {
+            num = scanner.nextInt();
+            sum += num;
+        } while (num >= 0);
+        System.out.println(sum);
+    }
+
+}
---
+++ Test2/test9.java
@@ -0,0 +1,20 @@
+package Test2;
+
+public class test9 {
+    public static void main(String[] args) {
+//        int i = -5;
+        int i = 5;
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
+
+        int j =-5;
+        System.out.println("Do while ");
+        do {
+            System.out.println(j);
+            j--;
+        } while (j > 0);
+    }
+
+}
