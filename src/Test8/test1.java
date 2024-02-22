e6e04e7eb4d52dc5d212122190deae7bf5f4e87b
Amarprit <amarpritkaur.kharbanda@citrix.com>
2024-02-22 23:38:48 +0530

    find missing element

--- src/Test8/test1.java
+++ src/Test8/test1.java
@@ -2,6 +2,20 @@ package src.Test8;
 
 public class test1 {
     public static void main(String[] args) {
+        int[] arr1 = {1, 4, 5, 7, 9};
+        int[] arr2 = {4, 5, 7, 9};
+        findMissingElement(arr1, arr2);
 
+    }
+
+    private static void findMissingElement(int[] a1, int[] a2) {
+        int missingElement = 0;
+        for (int i = 0; i < a1.length; i++) {
+            missingElement ^= a1[i];
+        }
+        for (int j = 0; j < a2.length; j++) {
+            missingElement ^= a2[j];
+        }
+        System.out.println(missingElement);
     }
 }
